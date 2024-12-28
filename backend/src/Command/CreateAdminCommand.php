cb5a1f1ab61e82f2a232dece5e75e30d28cc1a2c
Daniil Yaroshuk <d.yaroshuk@syberry.com>
2024-12-28 05:44:34 +0300

    Deploy to production with create admin

--- backend/src/Command/CreateAdminCommand.php
+++ backend/src/Command/CreateAdminCommand.php
@@ -12,6 +12,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
@@ -27,19 +28,29 @@ class CreateAdminCommand extends Command
         parent::__construct($name);
     }
 
+    protected function configure()
+    {
+        $this->addOption('generate-random-password', 'r', InputOption::VALUE_OPTIONAL, 'Generate random password', false);
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $email = 'admin@admin.test';
-        $password = '12345';
+        $password = $input->getOption('generate-random-password') ? substr(sha1((string) microtime(true)), 0, 8) : '12345';
 
         $user = $this->userRepository->findOneBy(['email' => $email]);
 
         if (is_null($user)) {
             $user = new User();
             $user->setEmail($email);
-            $user->setPassword($this->userPasswordHasher->hashPassword($user, $password));
             $user->setRoles([UserRoleEnum::ROLE_ADMIN->value, UserRoleEnum::ROLE_USER->value]);
+            $user->lastName = 'The';
+            $user->firstName = 'ISMI';
+            $user->patronymic = 'Admin';
+            $user->iin = '000000000000';
             $this->entityManager->persist($user);
+        } else {
+            $user->setPassword($this->userPasswordHasher->hashPassword($user, $password));
         }
 
         $this->entityManager->flush();
